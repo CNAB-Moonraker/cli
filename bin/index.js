f618be0b9ea23d45373a3b92d64169aa874dc9a4
Chance Murray <chance.murray@domo.com>
2020-01-25 10:32:39 -0700

    more setup

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log("Hello from my CLI!");
+const chalk = require('chalk');
+const moonraker = require('commander');
+
+moonraker
+  .command('hello')
+  .option('-n, --name <name>', 'Pass a variable to the CLI.')
+  .action((name="from my CLI") => {
+    console.log(chalk.cyan(`Hello ${name}!`));
+  })
+  
+
+moonraker.parse(process.argv)
