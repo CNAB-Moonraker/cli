ee27c79abd5a42fd880f5967c88f4047e2811018
madisonbc17 <madisonbc17@gmail.com>
2020-01-27 09:49:03 -0700

    Adding setup command and a fun command to CLI.

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,8 @@
 const chalk = require('chalk');
 const didYouMean = require('didyoumean');
 const program = require('commander');
-const { logo, runServer } = require('../utils');
+const { logo, runServer, funMessage, setupFrontend } = require('../utils');
+
 
 program
   .name('moonraker')
@@ -15,6 +16,19 @@ program
   .option('-p, --port <port>', 'Optional Port Parameter - Defaults to 3002')
   .action(runServer)
 
+//program command setup
+program
+  .command('setup')
+  .option('-l, --filepath <filepath>', 'Define the local filepath to show local visualization')
+  //this option not working yet, but is for future use once we integrate the cloud bundle
+  //FIX ME:
+  //.option('.c', 'Get your visualization from bundles installed across multiple devices (cloud)')
+  .action(setupFrontend)
+
+//for fun :)
+program
+  .command('arrg')
+  .action(funMessage)
 
 // Handle unknown command
 program
