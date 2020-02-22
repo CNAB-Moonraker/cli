b9a6dac222d5edf3b34e47b15cf596a01d9b1035
Gabe Coelho <gabrielpalmacoelho@gmail.com>
2020-02-22 11:05:32 -0700

    docker-app files are now read

--- server/routes/claims.js
+++ server/routes/claims.js
@@ -9,28 +9,52 @@ let response = {};
 
 response['porter'] = []
 response['duffle'] = []
+response['docker-app'] = []
 
 const porterDir = path.resolve(os.homedir(), '.porter/claims')
 const duffleDir = path.resolve(os.homedir(), '.duffle/claims')
+const dockerappDir = path.resolve(os.homedir(), '.docker/app/installations/claims')
 
 // Read porter directory
 fs.readdir(porterDir, (err, data) => {
-    if (err) throw err;
-    data.forEach(async file => {
-        let fullFile = path.resolve(porterDir, file)
-        let contents = await readFile(fullFile)
-        response['porter'].push(JSON.parse(contents.toString()))
-    });
+    if (err) {
+        console.log("Porter is not installed.")
+    }
+    else {
+        data.forEach(async file => {
+            let fullFile = path.resolve(porterDir, file)
+            let contents = await readFile(fullFile)
+            response['porter'].push(JSON.parse(contents.toString()))
+        });
+    }
 });
 
+// Read duffle directory
 fs.readdir(duffleDir, (err, data) => {
-    if (err) throw err;
-    data.forEach(async file => {
-        let fullFile = path.resolve(duffleDir, file)
-        let contents = await readFile(fullFile)
-        response['duffle'].push(JSON.parse(contents.toString()))
-    })
+    if (err) {
+        console.log("Duffle claims not found.")
+    }
+    else {
+        data.forEach(async file => {
+            let fullFile = path.resolve(duffleDir, file)
+            let contents = await readFile(fullFile)
+            response['duffle'].push(JSON.parse(contents.toString()))
+        })
+    }
 });
+
+// Read dockerapp directory
+fs.readdir(dockerappDir, (err, data) => {
+    if (err) {
+        console.log("Docker-app claims not found.")
+    } else {
+        data.forEach(async file => {
+            let fullFile = path.resolve(porterDir, file)
+            let contents = await readFile(fullFile)
+            response['docker-app'].push(JSON.parse(contents.toString()))
+        })
+    }
+})
 
 router.get('/', function (req, res, next) {
     res.json(response);
